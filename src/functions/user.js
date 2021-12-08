cb06bda68d5e5f30a6bfc3807aabc97d0c2dd094
miaochenxi <miaochenxi@outlook.com>
2021-12-09 00:51:58 +0800

    看到此消息请重新手动打一次卡；提交参数匹配；

--- src/functions/user.js
+++ src/functions/user.js
@@ -249,7 +249,6 @@ exports.addAttributes = (obj, wj_type) => {
     obj.other = ''
     obj.no_yy = ''
     obj.no_jtyy = ''
-    obj.glztlb = ''
     obj.hb_date = ''
     obj.jz_qzbl = ''
     obj.tz_qzbl = ''
